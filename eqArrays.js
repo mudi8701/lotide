b857effc06ceb366a672ffee2fa042a985195fe7
mudiaga oputu <mudi8701@gmail.com>
2021-08-04 03:41:05 +0000

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`); // string literals
+    // console.log('Assertion Pass:' + actual + '===' + expected)
+    // string concatenate
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
